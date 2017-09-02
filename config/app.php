99c655ddeff140f72be03ab45a48f09d04c09837
dogstar <chanzonghuang@gmail.com>
2017-09-01 21:03:52 -0700

    config

---
+++ config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+    'PHPMailer' => array(
+        'email' => array(
+            'host' => 'smtp.gmail.com',
+            'username' => 'XXX@gmail.com',
+            'password' => '******',
+            'from' => 'XXX@gmail.com',
+            'fromName' => 'PhalApi团队',
+            'sign' => '<br/><br/>请不要回复此邮件，谢谢！<br/><br/>-- PhalApi团队敬上 ',
+        ),
+    ),
+);
