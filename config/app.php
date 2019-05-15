a9dccd4d204cdad98fa36c6e414b4e1999fe50fa
dogstar <411170161@qq.com>
2019-05-15 17:34:45 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -6,6 +6,7 @@ return array(
             'host' => 'smtp.gmail.com',
             'username' => 'XXX@gmail.com',
             'password' => '******',
+            'port' => 25, // 端口
             'from' => 'XXX@gmail.com',
             'fromName' => 'PhalApi团队',
             'sign' => '<br/><br/>请不要回复此邮件，谢谢！<br/><br/>-- PhalApi团队敬上 ',
